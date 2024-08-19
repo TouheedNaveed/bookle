1baf0eb10e12b8ff1ab574a240667c591d843f04
Touheed Naveed <braja7059@gmail.com>
2024-08-19 17:05:29 +0500

    complete project with slide bar

--- proj 1/script.js
+++ proj 1/script.js
@@ -5,18 +5,25 @@ document.getElementById('openSidebarButton').addEventListener('click', function(
   document.getElementById('closeSidebarButton').addEventListener('click', function() {
     document.getElementById('sidebar').classList.add('translate-x-full');
   });
+  
+
   document.querySelectorAll('.expand').forEach(button => {
     button.addEventListener('click', function(event) {
       event.preventDefault();
-      const submenu = this.previousElementSibling;
+      const submenu = this.closest('li').querySelector('.submenu');
   
-      // Toggle the hidden class
-      submenu.classList.toggle('hidden');
-  
-      // Change the icon from plus to minus
-      this.innerHTML = submenu.classList.contains('hidden') 
-        ? '<i class="far fa-plus"></i>' 
-        : '<i class="fas fa-times"></i>';
+      // Toggle the hidden class and expand/collapse the submenu
+      if (submenu.classList.contains('hidden')) {
+        submenu.classList.remove('hidden');
+        submenu.style.maxHeight = submenu.scrollHeight + "px"; // Expand
+        this.innerHTML = '<i class="fas fa-times"></i>'; // Change to cross icon
+      } else {
+        submenu.style.maxHeight = "0px"; // Collapse
+        submenu.addEventListener('transitionend', () => {
+          submenu.classList.add('hidden');
+        }, { once: true });
+        this.innerHTML = '<i class="far fa-plus"></i>'; // Change back to plus icon
+      }
     });
   });
-    
+  
